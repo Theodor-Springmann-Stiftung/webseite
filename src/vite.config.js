3b74f56c6615a658a50148bdeba2b74a3f3cda8c
Simon Martens <simon.martens@mailbox.org>
2024-10-17 14:37:54 +0200

    Configured build

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,9 +1,15 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+
 export default defineConfig({
   build: {
-    root: resolve(__dirname, "src"),
     outDir: "./dist/",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        impressum: resolve(__dirname, "impressum", "index.html"),
+      },
+    },
   },
 });
