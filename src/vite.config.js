fb17739ab3909823cc8a2b6ef888119d969998d0
Simon Martens <simon.martens@mailbox.org>
2026-06-01 18:22:45 +0200

    CHORE: bump deps

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,7 +1,9 @@
 import { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  plugins: [tailwindcss()],
   build: {
     outDir: "../dist/",
     emptyOutDir: true,
